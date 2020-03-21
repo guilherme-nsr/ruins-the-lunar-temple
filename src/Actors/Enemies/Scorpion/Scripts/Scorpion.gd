8cd96d4575f92c90e8ae3638e3f11e233b446ec4
Guilherme Ribeiro <desaribeiro.guilherme@gmail.com>
2020-03-20 22:42:02 -0300

    Handling Player's collision with enemies

--- src/Actors/Enemies/Scorpion/Scripts/Scorpion.gd
+++ src/Actors/Enemies/Scorpion/Scripts/Scorpion.gd
@@ -16,3 +16,8 @@ func _physics_process(delta: float) -> void:
 		_velocity.x *= -1.0;
 	$AnimatedSprite.flip_h = _velocity.x < 0;
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y;
+
+
+func _on_PlayerDetector_body_entered(body: Node) -> void:
+	if body.get_filename().get_file() == "Player.tscn":
+		body.position = Vector2(75.0, 250.0)
